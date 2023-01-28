8e565df583021cb6abaaa5d0f927c20136898a5d
Anderson Armindo João <andersonarmindojoao@outlook.com>
2023-01-28 13:49:37 +0200

    Add
    Criacao do frontend

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -13,26 +13,48 @@
       </q-toolbar>
       <div class="q-px-lg q-pt-xl q-mb-md">
         <div class="text-h3">Todo</div>
-        <div class="text-subtitle1">Segunda-feira</div>
+        <div class="text-subtitle1">{{ todaysdate }}</div>
       </div>
+      <q-img src="../../public/sunset.jpg" class="header-image absolute-top" />
     </q-header>
 
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
-      bordered
-      content-class="bg-grey-1"
+      :width="200"
+      :breakpoint="600"
     >
-      <q-list>
-        <q-item-label header class="text-grey-8">
-          Essential Links
-        </q-item-label>
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+      <q-scroll-area
+        style="
+          height: calc(100% - 192px);
+          margin-top: 192px;
+          border-right: 1px solid #ddd;
+        "
+      >
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
+
+            <q-item-section> Todo </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <q-img
+        class="absolute-top"
+        src="../../public/sunset.jpg"
+        style="height: 192px"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="../../public/Perfil.jpg" />
+          </q-avatar>
+          <div class="text-weight-bold">Anderson Armindo</div>
+          <div>@andersonarmindo</div>
+        </div>
+      </q-img>
     </q-drawer>
 
     <q-page-container>
@@ -42,6 +64,8 @@
 </template>
 
 <script>
+import { date } from "quasar";
+
 import EssentialLink from "components/EssentialLink.vue";
 
 const linksData = [
@@ -100,5 +124,20 @@ export default {
       essentialLinks: linksData,
     };
   },
+  computed: {
+    todaysdate() {
+      const timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd D MMMM ");
+    },
+  },
 };
 </script>
+
+<style lang="scss">
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.2;
+  filter: grayscale(100%);
+}
+</style>
